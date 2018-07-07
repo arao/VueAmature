37c2f8c44aa3e30b474f5d96fed583efe8b66d27
akhilesh <akhileshrao19@gmail.com>
2018-07-08 04:08:08 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
-var one = new Vue({
-    el: "#vue-app-one",
-    data:{
-        title: "Vue App One"
+Vue.component('greeting', {
+    template: '<p> {{name}} is here. <button v-on:click="changeName"> Change Name </button></p>',
+    data : function(){
+        return {
+            name : "Yashida"
+        }
     },
-    methods:{
-
-    },
-    computed:{
-        greet: function(){
-            return "First Welcome You Terator";
+    methods :{
+        changeName : function () {
+            this.name = "KoKilo"
         }
     }
+});
+
+var one = new Vue({
+    el: "#vue-app-one",
+
 });
 
 var two = new Vue({
     el: "#vue-app-two",
-    data:{
-        title: "Vue App Two"
-    },
-    methods:{
-        changeTitle: function(){
-            one.title = "Title Changed";
-        }
-    },
-    computed:{
-        greet: function(){
-            return "Second Welcome You Terator";
-        }
-    }
+
 });
+
